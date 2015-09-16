31c592889ceac80896c746d4f98fdd1e8d812604
Daniel Nüst <d.nuest@52north.org>
2015-09-16 09:40:58 +0200

    use 52N parent pom, add API interfaces

---
+++ src/main/java/org/n52/youngs/control/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2015-${currentYearDynamic} 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.n52.youngs.control;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author <a href="mailto:d.nuest@52north.org">Daniel Nüst</a>
+ */
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+    }
+
+}
